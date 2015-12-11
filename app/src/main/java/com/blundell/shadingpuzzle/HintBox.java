255571ea4982f95bfdd7f8b3c92adbb212d6edff
blundell <blundell.app.dev@gmail.com>
2015-12-11 12:58:52 +0000

    remove unused code

--- app/src/main/java/com/blundell/shadingpuzzle/HintBox.java
+++ app/src/main/java/com/blundell/shadingpuzzle/HintBox.java
@@ -6,35 +6,16 @@ import android.widget.TextView;
 
 public class HintBox extends TextView implements ZoomablePuzzle.Zoomable {
 
-    private float scaleFactor = 1;
     private float originalTextSize;
 
     public HintBox(Context context) {
         super(context);
-        init();
-    }
-
-    private void init() {
         setTextColor(Color.BLACK);
         originalTextSize = getTextSize();
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-//        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-//
-//        int zoomedWidth = (int) (widthSize * scaleFactor);
-//        int zoomedHeight = (int) (heightSize * scaleFactor);
-//
-//        setMeasuredDimension(zoomedWidth, zoomedHeight);
-    }
-
-    @Override
     public void adjustZoom(float scaleFactor) {
-        this.scaleFactor = scaleFactor;
         setTextSize(originalTextSize * scaleFactor);
     }
 }
