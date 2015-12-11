010669f01717d3c9942fc6a63fcf451cccba74b4
blundell <blundell.app.dev@gmail.com>
2015-12-11 12:58:17 +0000

    checkboxes can now zoom

--- app/src/main/java/com/blundell/shadingpuzzle/LightBox.java
+++ app/src/main/java/com/blundell/shadingpuzzle/LightBox.java
@@ -11,39 +11,20 @@ public class LightBox extends CheckBox implements ZoomablePuzzle.Zoomable {
     private final Paint onPaint = new Paint();
     private final Paint offPaint = new Paint();
 
-    private float scaleFactor = 1;
-    private int normPaddingLeft;
-    private int normPaddingTop;
-    private int normPaddingRight;
-    private int normPaddingBottom;
     private int widthSize;
     private int heightSize;
 
     public LightBox(Context context) {
         super(context);
-        init();
-    }
-
-    private void init() {
         onPaint.setColor(Color.BLACK);
         offPaint.setColor(Color.WHITE);
-        normPaddingLeft = getPaddingLeft();
-        normPaddingTop = getPaddingTop();
-        normPaddingRight = getPaddingRight();
-        normPaddingBottom = getPaddingBottom();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        heightSize = MeasureSpec.getSize(heightMeasureSpec);
-//
-//        int zoomedWidth = (int) (widthSize * scaleFactor);
-//        int zoomedHeight = (int) (heightSize * scaleFactor);
-//
-//        setMeasuredDimension(zoomedWidth, zoomedHeight);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        widthSize = getMeasuredWidth();
+        heightSize = getMeasuredHeight();
     }
 
     @Override
@@ -58,10 +39,7 @@ public class LightBox extends CheckBox implements ZoomablePuzzle.Zoomable {
 
     @Override
     public void adjustZoom(float scaleFactor) {
-        this.scaleFactor = scaleFactor;
-//        setWidth((int) (widthSize * scaleFactor));
-//        setHeight((int) (heightSize * scaleFactor));
-        invalidate();
+        setMeasuredDimension((int) (widthSize * scaleFactor), (int) (heightSize * scaleFactor));
     }
 
     public void setAlwaysDimmed() {
